a24beac02ff05c97ca1fddbe046177f962eaf1ec
timothy-ahn <tim.ahn@outlook.com>
2024-12-03 00:31:12 +0500

    cp

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -108,7 +108,7 @@ def get_dealer_reviews(request, dealer_id):
         for review_detail in reviews:
             # response = analyze_review_sentiments(review_detail['review'])
             # print(response)
-            review_detail['sentiment'] = 10 #response['sentiment']
+            review_detail['sentiment'] = 50 # = response['sentiment']
         return JsonResponse({"status":200,"reviews":reviews})
     else:
         return JsonResponse({"status":400,"message":"Bad Request"})
